fc50c216c90065f13c2a5a6ab68121393e30c523
piyush kumar <{piyushsharma1155@gmail.com}+{piyush2040}@users.noreply.github.com>
2021-10-22 20:25:00 +0530

    login page design

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Food-order</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- Optional theme -->
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="front-css">
+<!--from here starts heading-->
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand logo" href="index.php">Food-order</a>
+<a class="cust-profile" href="cust-profile.php">Piyush Kumar</a>
+</nav>
+<p id="punch-line">Discover the best resturant food in your city</p>
+<div class="search-box">
+  
+    <input type="search" id="search-input" class="form-control" placeholder="search food or resturant"/>
+
+  <button type="button" class="btn btn-primary" id="search-button">
+    <i class="fa fa-search"></i>
+  </button>
+</div>
+</div>
+<div id="result1"></div>
+<!-- Footer -->
+<footer class="page-footer font-small indigo">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Very long link 1</a>
+          </li>
+          <li>
+            <a href="#!">Very long link 2</a>
+          </li>
+          <li>
+            <a href="#!">Very long link 3</a>
+          </li>
+          <li>
+            <a href="#!">Very long link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <div class="container">
+  <!-- Footer Links -->
+  <div class="row">
+
+<!-- Grid column -->
+<div class="col-md-12">
+  <div class="md-5">
+
+    <!-- Facebook -->
+    <a class="fb-ic">
+      <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+    </a>
+    <!-- Twitter -->
+    <a class="tw-ic">
+      <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+    </a>
+    <!-- Google +-->
+    <a class="gplus-ic">
+      <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+    </a>
+    <!--Linkedin -->
+    <a class="li-ic">
+      <i class="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+    </a>
+    <!--Instagram-->
+    <a class="ins-ic">
+      <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+    </a>
+    <!--Pinterest-->
+    <a class="pin-ic">
+      <i class="fa fa-pinterest fa-lg white-text fa-2x"> </i>
+    </a>
+  </div>
+</div>
+<!-- Grid column -->
+
+</div>
+<!-- Grid row-->
+
+</div>
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">By continuing past this page, you agree to our Terms of Service, Cookie Policy, Privacy Policy and Content Policies. All trademarks are properties of their respective owners. 2008-2021 © food-order™ Ltd. All rights reserved.
+    
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</body>
+
+</html>
---
+++ login-ask.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Food-order</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- Optional theme -->
+
+<link rel="stylesheet" type="text/css" href="css/style-for-login-ask.css">
+</head>
+<body>
+<div class="d-flex flex-column bd-highlight mb-3">
+<div class="p-2 bd-highlight"><a>Login as a customer</a></div>
+<div class="p-2 bd-highlight"><a>Login as a resturant</a></div>
+<div class="p-2 bd-highlight"><a>Login as a delivery executive</a></div>
+</div>
+</body>
+</html>
